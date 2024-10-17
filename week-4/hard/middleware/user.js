ba40f2c5da98df9e35a61d76b04e073d7aebd934
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2024-10-17 19:35:56 +0530

    resolve all issue

--- week-4/hard/middleware/user.js
+++ week-4/hard/middleware/user.js
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken')
 
 const { JWT_USER, JWT_ADMIN } = require('../config');
 
-||||||| parent of c556ad3 (week4 assgnment done but not hard one)
-=======
-const jwt = require('jsonwebtoken')
-console.log(require('../config'));
-
-const { JWT_USER, JWT_ADMIN } = require('../config');
-
-console.log("JWT_USER in middleware:", JWT_USER);
-console.log("JWT_ADMIN in middleware:", JWT_ADMIN);
-
->>>>>>> c556ad3 (week4 assgnment done but not hard one)
 function userMiddleware(req, res, next) {
     // Implement user auth logic
-<<<<<<< HEAD
     
     const token = req.headers.token 
     const decoded = jwt.verify(token, JWT_USER)
@@ -31,26 +18,8 @@ function userMiddleware(req, res, next) {
     }
     
   
-||||||| parent of c556ad3 (week4 assgnment done but not hard one)
-=======
-    
-    const token = req.headers.token
-    const decoded = jwt.verify(token, JWT_USER)
-
-    if(decoded) {
-        req.userId = decoded.id;
-        next()
-    }else{
-        res.status(403).json({
-            message: "user not found"
-        })
-    }
-    
-  
->>>>>>> c556ad3 (week4 assgnment done but not hard one)
 }
 
-<<<<<<< HEAD
 function adminMiddleware(req, res, next) {
     const token = req.headers.token
     const decoded = jwt.verify(token, JWT_ADMIN);
@@ -69,25 +38,3 @@ module.exports = {
     userMiddleware,
     adminMiddleware
 }
-||||||| parent of c556ad3 (week4 assgnment done but not hard one)
-module.exports = userMiddleware;
-=======
-function adminMiddleware(req, res, next) {
-    const token = req.headers.token
-    const decoded = jwt.verify(token, JWT_ADMIN);
-
-    if (decoded) {
-        req.adminId = decoded.id;
-        next();
-    }else{
-        res.status(403).json({
-            message: "YOU are not signed in"
-        })
-    }
-}
-
-module.exports = {
-    userMiddleware,
-    adminMiddleware
-}
->>>>>>> c556ad3 (week4 assgnment done but not hard one)
