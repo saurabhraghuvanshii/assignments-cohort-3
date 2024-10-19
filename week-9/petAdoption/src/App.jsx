8462fa19f102b9d88c1e456d3c9996b5be090cb0
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2024-10-19 16:12:26 +0530

    done week9 petadoption

--- week-9/petAdoption/src/App.jsx
+++ week-9/petAdoption/src/App.jsx
@@ -1,20 +1,44 @@
 import Header from './components/Header';
 import PetAdoptionForm from './components/PetAdoptionForm';
 import "./myApp.css";
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PetAdoptionForm from "./components/PetAdoptionForm";
+import TableData from "./components/TableData";
 
 const App = () => {
-  return (
-    <div
-      style={{
-        backgroundImage: "url('https://images.unsplash.com/photo-1450778869180-41d0601e046e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1950&q=80')",
-        height: "100vh",
-        backgroundSize: "cover"
-      }}
-    >
-      <Header message={"Pet Adoption Form"} />
-      <PetAdoptionForm />
-    </div>
-  );
+	const [submissions, setSubmissions] = useState([]);
+  
+  const appStyle = {
+    width: "100%",
+    backgroundImage: 'url("https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg")',
+    backgroundSize: "cover",
+    height: "100vh",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center", // Center content horizontally
+    alignItems: "center", // Center content vertically
+    padding: "20px", // To add some space around the content
+  };
+  
+
+	return (
+		<div style={appStyle}>
+      
+			<Router>
+				<Routes>
+					<Route
+						path="/"
+						element={<PetAdoptionForm setSubmissions={setSubmissions} />}
+					/>
+					<Route path="/table" element={<TableData submissions={submissions} />} />
+				</Routes>
+			</Router>
+      
+		</div>
+	);
 };
+
 export default App;
