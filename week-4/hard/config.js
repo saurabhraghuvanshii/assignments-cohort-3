d8db1c762c1419edb2ebfacbb5436243662dc0b3
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2024-10-29 22:33:16 +0530

    done

--- week-4/hard/config.js
+++ week-4/hard/config.js
@@ -1,23 +1,12 @@
 require('dotenv').config();
-const JWT_USER = process.env.JWT_USER
-const JWT_ADMIN = process.env.JWT_ADMIN
 
+const JWT_USER = process.env.JWT_USER;
+const JWT_ADMIN = process.env.JWT_ADMIN;
+
+console.log(JWT_ADMIN);
+console.log("JWT_USER:", JWT_USER);
 
 module.exports = {
     JWT_USER,
     JWT_ADMIN
 };
-const JWT_USER = process.env.JWT_USER
-const JWT_ADMIN = process.env.JWT_ADMIN
-console.log(JWT_ADMIN);
-console.log("JWT_USER:", JWT_USER);
-
-console.log(JWT_ADMIN);
-console.log("JWT_USER:", JWT_USER);
-
-
-module.exports = {
-    JWT_USER,
-    JWT_ADMIN
-}
-
