b3e7e6eed6b213791975ebe2cbdfc1b4056bbd63
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2024-10-29 22:33:16 +0530

    done

--- week-4/hard/config.js
+++ week-4/hard/config.js
@@ -1,40 +1,23 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 require('dotenv').config();
 const JWT_USER = process.env.JWT_USER
 const JWT_ADMIN = process.env.JWT_ADMIN
-<<<<<<< HEAD
 
 
 module.exports = {
     JWT_USER,
     JWT_ADMIN
 };
-||||||| parent of 0602141 (complete login)
-=======
-||||||| parent of eb9ff86 (check for undefined)
-=======
-require('dotenv').config();
->>>>>>> eb9ff86 (check for undefined)
 const JWT_USER = process.env.JWT_USER
 const JWT_ADMIN = process.env.JWT_ADMIN
 console.log(JWT_ADMIN);
 console.log("JWT_USER:", JWT_USER);
-||||||| parent of fe41d16 (resolve all issue)
-console.log( JWT_ADMIN);
-console.log("JWT_USER:", JWT_USER);
-=======
 
->>>>>>> fe41d16 (resolve all issue)
+console.log(JWT_ADMIN);
+console.log("JWT_USER:", JWT_USER);
+
 
 module.exports = {
     JWT_USER,
     JWT_ADMIN
-<<<<<<< HEAD
 }
->>>>>>> 0602141 (complete login)
-||||||| parent of eb9ff86 (check for undefined)
-}
-=======
-};
->>>>>>> eb9ff86 (check for undefined)
+
