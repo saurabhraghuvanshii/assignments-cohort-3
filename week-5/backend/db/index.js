c27905c5fed42a7eb69def533b5ce53d33ef9661
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2024-12-18 20:19:08 +0530

    week 5 change

--- week-5/backend/db/index.js
+++ week-5/backend/db/index.js
@@ -1,1 +1,34 @@
-//  start writing from here
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const connectToDb = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Database connected");
+    } catch (err) {
+        console.error("connection failed:", err); 
+    }
+}
+
+const UserSchema = new Schema({
+    username: String,
+    password: String,
+});
+
+const TodoSchema = new Schema({
+    userId: ObjectId,
+    title: String,
+    completed: Boolean,
+});
+
+const User = mongoose.model('User', UserSchema);
+const Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports = {
+    connectToDb,
+    User,
+    Todo
+}
--- week-5/backend/middleware/user.js
+++ week-5/backend/middleware/user.js
@@ -1,1 +1,25 @@
-//  start writing from here
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const USER_SECRET = process.env.USER_SECRET
+console.log(USER_SECRET);
+
+const authenticateJwt = (req, res, next) => {
+    const authHeader = req.heasders.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, USER_SECRET, (err, user)=>{
+            if (err) {
+                return res.status(403).json({ message: "Invaild token"});
+            }
+            req.userId = user.userId;
+            next();
+        });
+    }else{
+        res.status(401).json({ message: 'No token provided'});
+    }
+};
+
+module.exports = {
+    authenticateJwt,
+    USER_SECRET
+};
